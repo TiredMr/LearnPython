0ddc8d67bbff8dad8e94dff8823afc7b9ce8f219
TiredMr <ffabricio047@gmail.com>
2025-01-22 04:50:29 -0300

    Running

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ my_name = "Tired"
 #Print 
 print("Hello and welcome " + my_name + "!")
 
+class MeuPerfil:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def mostrar_nome(self):
+        print("Meu nome é: " + self.nome)
+    
+    def mostrar_idade(self):
+        print("Eu tenho: " + self.idade)
+
+x = MeuPerfil('Fabricio', '21')
+
+MeuPerfil.mostrar_nome(self=x)
